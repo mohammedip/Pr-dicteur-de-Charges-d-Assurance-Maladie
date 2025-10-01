53a9d2799129b247a3172d41a8b9edd5108a6958
mohammedip <reidamohammed3@gmail.com>
2025-10-01 10:57:58 +0100

    PDCDM-3 Gérer valeurs manquantes & doublons PDCDM-4, Détection/traitement outliers PDCDM-5, Encodage catégorielles + normalisation PDCDM-6, Split train/test PDCDM-7

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,2 +1,46 @@
-from data import df
+from data import df , sns ,plt , pd , np
+from scipy import stats
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+
+
+df_new = df.drop_duplicates()
+
+Q1 = df_new['bmi'].quantile(0.25)
+
+Q3 = df_new['bmi'].quantile(0.75)
+
+IQR = Q3 - Q1
+
+lower = Q1 - 1.5 * IQR
+upper = Q3 + 1.5 * IQR
+
+df_no_outliers = df_new[(df_new["bmi"] >= lower) & (df_new["bmi"] <= upper)]
+
+df_no_outliers["log_charges"] = np.log(df_no_outliers["charges"])
+
+df_encoded = pd.get_dummies(df_no_outliers, columns=["sex", "smoker", "region"], dtype=int)
+
+df_encoded = df_encoded.drop("charges", axis=1)
+
+
+num_cols = ["age", "bmi", "children", "log_charges"]
+
+scaler = StandardScaler()
+df_scaled = df_encoded.copy()
+df_scaled[num_cols] = scaler.fit_transform(df_encoded[num_cols])
+
+
+
+X = df_scaled.drop("log_charges", axis=1)   
+y = df_scaled["log_charges"]            
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y,
+    test_size=0.2,      
+    random_state=42,     
+    shuffle=True       
+)
+
+
 
